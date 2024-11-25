2216d0a601bef38f71da0ff278d8397c93cdd93c
wulliaopolol <145827057+wulliaopolol@users.noreply.github.com>
2024-11-25 19:04:09 +0800

    kakak

--- src/Aimbot.lua
+++ src/Aimbot.lua
@@ -107,7 +107,7 @@ getgenv().ExunysDeveloperAimbot = {
 		Sensitivity2 = 3.5, -- mousemoverel Sensitivity
 
 		LockMode = 1, -- 1 = CFrame; 2 = mousemoverel
-		LockPart = "Torso", -- Body part to lock on
+		LockPart = "Head", -- Body part to lock on
 
 		TriggerKey = Enum.UserInputType.MouseButton2,
 		Toggle = false
